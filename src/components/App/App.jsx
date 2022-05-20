8962530dee6d42302f6a4cc8a0f7ca8c2ca3ff4d
tariq-almalki <e.tariq.almalki@gmail.com>
2022-05-20 22:46:05 +0300

    add persistent state for darkmode using local storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import { ParticlesBackground } from '../ParticlesBackground/ParticlesBackground.jsx';
 import { Container } from '../Container/Container.jsx';
 import { CardExtended } from '../CardExtended/CardExtended.jsx';
-import { useState } from 'react';
+import GithubCorner from 'react-github-corner';
+import { useLocalStorage } from '../../hooks/useLocalStorage.jsx';
 
 export function App() {
-    const [isDarkMode, setIsDarkMode] = useState(true);
+    // const [isDarkMode, setIsDarkMode] = useState(true);
+    const [isDarkMode, setIsDarkMode] = useLocalStorage('isDarkMode', true);
 
     function isCheckedHandler(checked) {
         setIsDarkMode(checked);
@@ -14,6 +16,11 @@ export function App() {
         <Container>
             <ParticlesBackground isDarkMode={isDarkMode} />
             <CardExtended isDarkMode={isDarkMode} onCheckedHandler={isCheckedHandler} />
+            <GithubCorner
+                octoColor={isDarkMode ? '#151513' : '#fff'}
+                bannerColor={isDarkMode ? '#fff' : '#151513'}
+                href='https://github.com/kiemkist/temperature-converter'
+            />
         </Container>
     );
 }
