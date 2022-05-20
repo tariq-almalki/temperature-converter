dd3483bd973fbf0e924307fd9e8785079ade13bc
tariq-almalki <e.tariq.almalki@gmail.com>
2022-05-21 00:09:10 +0300

    replace flex with grid in GridItemThree Component

--- src/components/CardExtended/Grid-Container_Components/GridItemThree/GridItemThree.StyledComponent.jsx
+++ src/components/CardExtended/Grid-Container_Components/GridItemThree/GridItemThree.StyledComponent.jsx
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
 
 export const Div = styled.div`
-    display: flex;
-    justify-content: space-around;
+    display: grid;
+    /* grid-gap: 5px; */
+    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
     align-items: center;
+    justify-items: center;
 `;
 
 export const H1 = styled.h1`
     font-family: 'Roboto', sans-serif;
+    width: fit-content;
 `;
